cb000c866eb9c8d14549b458ca8902e947ff8168
Bernhard Behrendt <bernhard.bezdek@googlemail.com>
2024-05-30 11:43:59 +0200

    feat: set fixed env type

--- src/utils/getEnvironment.js
+++ src/utils/getEnvironment.js
@@ -1,21 +1,11 @@
-const isElectron = require('is-electron');
-
 module.exports = (key) => {
-  const env = {};
+    const env = {};
 
-  if (typeof WorkerGlobalScope !== 'undefined') {
-    env.type = 'webworker';
-  } else if (isElectron()) {
-    env.type = 'electron';
-  } else if (typeof document === 'object') {
-    env.type = 'browser';
-  } else if (typeof process === 'object' && typeof require === 'function') {
     env.type = 'node';
-  }
 
-  if (typeof key === 'undefined') {
-    return env;
-  }
+    if (typeof key === 'undefined') {
+        return env;
+    }
 
-  return env[key];
+    return env[key];
 };
